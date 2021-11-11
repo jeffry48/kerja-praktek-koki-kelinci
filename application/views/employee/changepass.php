810612def8807752beb09bc28e9828d2d63ebee2
michaelirawan1818 <84262977+michaelirawan1818@users.noreply.github.com>
2021-11-11 14:00:35 +0700

    Add files via upload

--- application/views/employee/changepass.php
+++ application/views/employee/changepass.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Update Password</title>
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <link rel="stylesheet" href="<?= base_url() ?>assets/backend/css/public/adminlte/bower_components/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?= base_url() ?>assets/backend/css/public/adminlte/bower_components/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?= base_url() ?>assets/backend/css/public/adminlte/bower_components/Ionicons/css/ionicons.min.css">
-    <link rel="stylesheet" href="<?= base_url() ?>assets/backend/css/public/adminlte/dist/css/AdminLTE.css">
-    <link rel="stylesheet" href="<?= base_url() ?>assets/backend/css/public/adminlte/plugins/iCheck/square/blue.css">
-    <link rel="stylesheet" href="<?= base_url() ?>assets/backend/css/public/adminlte/dist/css/skins/_all-skins.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Kategori</title>
+
+        <!-- Google Font: Source Sans Pro -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="<?= base_url() ?>assets/backend/css/adminlte/plugins/fontawesome-free/css/all.min.css">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="<?= base_url() ?>assets/backend/css/adminlte/dist/css/adminlte.min.css">
+        <link rel="stylesheet" href="<?= base_url() ?>assets/backend/css/public/adminlte/bower_components/font-awesome/css/font-awesome.min.css">
     <style>
         table {
           font-family: arial, sans-serif;
@@ -31,29 +31,27 @@
         tr:nth-child(even) {
           background-color: #dddddd;
         }
+        a{
+            color: white;
+        }
     </style>
-</head>
-<body class="hold-transition skin-blue sidebar-mini sidebar-collapse">
-    <header class="main-header">
-            <?php include 'application/views/header.php'; ?>
-        </header>
-
-        <aside class="main-sidebar">
-            <?php include 'application/views/sidebar.php';?>
-        </aside>
-    <div class="wrapper">
-
-        <div class="content-wrapper">
+    </head>
+    <body class="hold-transition sidebar-mini sidebar-collapse">
+        <?php include 'application/views/header.php'; ?>
+        <?php include 'application/views/sidebar.php';?>
+        <div class="wrapper">
+            <!-- Navbar -->
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper">
+                <!-- Content Header (Page header) -->
                 <section class="content-header">
                     <div class="container-fluid">
                         <div class="row mb-2">
-                            <div class="col-sm-6">
-                                <h1>Ubah Password</h1>
-                                <br>
-                            </div>
                         </div>
                     </div><!-- /.container-fluid -->
                 </section>
+
+                <!-- Main content -->
                 <section class="content">
                     <div class="container-fluid">
                         <div class="row">
@@ -61,27 +59,26 @@
                                 <!-- general form elements -->
                                 <div class="card card-primary">
                                     <div class="card-header">
+                                        <h3 class="card-title">Ubah Password</h3>
                                     </div>
                                     <!-- /.card-header -->
                                     <!-- form start -->
-                                    <form action = "<?= base_url() ?>UbahPass" method = "post">
+                                    <form action="<?= base_url() ?>UbahPass" method="post">
                                         <div class="card-body">
                                             <div class="form-group">
                                                 <label for="nama">Password Lama</label>
-                                                <input type="text" class="form-control" name="passlama" placeholder="Password Lama" >
+                                                <input type="text" name="passlama" class="form-control" id="nama" placeholder="Password Lama">
                                             </div>
                                             <div class="form-group">
-                                                <label for="alamat">Password Baru</label>
-                                                <input type="text" class="form-control" name="passbaru" placeholder="Password Baru">
+                                                <label for="nama">Password Baru</label>
+                                                <input type="text" name="passbaru" class="form-control" id="alamat" placeholder="Password Baru">
                                             </div>
-                                            
                                         </div>
                                         <!-- /.card-body -->
                                         <div class="card-footer">
                                             <button type="submit" class="btn btn-primary">Ubah</button>
                                         </div>
                                     </form>
-                                    <br>
                                 </div>
                                 <!-- /.card -->
                             </div>
@@ -89,31 +86,61 @@
                         <!-- /.row -->
                     </div><!-- /.container-fluid -->
                 </section>
-            </center>
+                <!-- /.content -->
+            </div>
+            <!-- /.content-wrapper -->
+            <!-- <footer class="main-footer">
+                <div class="float-right d-none d-sm-block">
+                    <b>Version</b> 3.2.0-rc
+                </div>
+                <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+            </footer> -->
+
+            <!-- Control Sidebar -->
+            <aside class="control-sidebar control-sidebar-dark">
+                <!-- Control sidebar content goes here -->
+            </aside>
+            <!-- /.control-sidebar -->
         </div>
+        <!-- ./wrapper -->
 
-        <!-- <footer class="main-footer">
-
-        </footer> -->
-        <!-- <div class="control-sidebar-bg"></div> -->
-    </div>
-
-    <script src="<?= base_url() ?>assets/backend/css/public/adminlte/bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="<?= base_url() ?>assets/backend/css/public/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="<?= base_url() ?>assets/backend/css/public/adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-    <script src="<?= base_url() ?>assets/backend/css/public/adminlte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js"></script>
-    <script src="<?= base_url() ?>assets/backend/css/public/adminlte/bower_components/fastclick/lib/fastclick.js"></script>
-    <script src="<?= base_url() ?>assets/backend/css/public/adminlte/dist/js/adminlte.min.js"></script>
-    <script src="<?= base_url() ?>assets/backend/css/public/adminlte/dist/js/demo.js"></script>
-
-    <script>
-        $(document).ready(function () {
-            $('.sidebar-menu').tree()
-        })
-
-        $(function () {
-            $('#edTanggal').datepicker();
-        });
-    </script>
-</body>
+        <!-- jQuery -->
+        <script src="<?= base_url() ?>assets/backend/css/adminlte/plugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="<?= base_url() ?>assets/backend/css/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- bs-custom-file-input -->
+        <script src="<?= base_url() ?>assets/backend/css/adminlte/plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="<?= base_url() ?>assets/backend/css/adminlte/dist/js/adminlte.min.js"></script>
+        <!-- Page specific script -->
+        <script>
+            $(function () {
+                bsCustomFileInput.init();
+                <?php
+                if(isset($_SESSION['success'])){
+                    echo '$("#myModal").modal("show");';
+                }
+                ?>
+            });
+        </script>
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="modalDetailLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalDetailLabel"></h5>
+            </div>
+            <div class="modal-body">
+                <?php 
+                echo $_SESSION['success']; 
+                $_SESSION['success']=null;
+                ?>        
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
+        </div>
+    </body>
 </html>
