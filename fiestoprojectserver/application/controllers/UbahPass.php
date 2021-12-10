fd022224f98badb666702cbc8b3297e9bec64978
michaelirawan1818 <84262977+michaelirawan1818@users.noreply.github.com>
2021-12-10 12:46:56 +0700

    Add files via upload

--- fiestoprojectserver/application/controllers/UbahPass.php
+++ fiestoprojectserver/application/controllers/UbahPass.php
@@ -3,12 +3,34 @@ class UbahPass extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
+        $this->load->model("Karyawan");
         $this->load->library('session');
+        $this->load->helper(array('cookie', 'url'));
+        // $this->load->helper('url');
+        // $this->load->view('employee/tambahpegawai.php');
     }
 
     public function index()
     {
-        $this->load->helper('URL');
-        $this->load->view('home/login.php');
+        // redirect(base_url() . 'login');
+        // $this->load->helper('URL');
+        // $this->load->helper('url');
+        // $this->load->view('employee/updatekategori.php');
+        $cookieData = get_cookie("active_id");
+        // echo $cookieData;
+        $passlama= $this->input->post('passlama');
+        $passbaru= $this->input->post('passbaru');
+        $cekpasslama = $this->Karyawan->cekPassLama($cookieData,$passlama);
+        // echo $cekpasslama;
+        if($cekpasslama > 0)
+        {
+            $updatepass = $this->Karyawan->updatepassbaru($cookieData,$passbaru);
+        }
+        ?>
+            <script type="text/javascript">
+                alert("Berhasil Ubah Password");
+            </script>
+        <?php
+        return redirect(base_url()."UbahPass");
     }
 }
